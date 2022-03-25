41aa4fcadbaf6b68f975c05da9cd31472f22da1f
Elevatedpeace <Peace08182443958>
2022-03-25 08:24:28 +0100

    cool

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -38,10 +38,11 @@ const Welcome = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Stake your <br />  assets on KokoToken
+            Stake your <br /> assets on KokoToken
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-         Buy, sell and swap for other cryptocurrencies. Stake your coins and earn 1% every 7 days
+            Buy, sell and swap for other cryptocurrencies. Stake your coins and
+            earn 1% every 7 days
           </p>
           {!currentAccount && (
             <button
@@ -55,7 +56,13 @@ const Welcome = () => {
               </p>
             </button>
           )}
-
+          <button
+            type="button"
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
+            <AiFillPlayCircle className="text-white mr-2" />
+            <p className="text-white text-base font-semibold">Hire me</p>
+          </button>
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
               Reliability
